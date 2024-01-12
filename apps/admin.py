3ff9e3d961d4eb776b6e82f6f841f89df31a8ee4
izzatulloh2006 <zzatullohtoshqandi@gmail.com>
2024-01-12 11:37:23 +0500

    Initial commit

---
+++ apps/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from apps.models import Blog, Tag , Category, Comment
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+
+
---
+++ apps/views.py
@@ -0,0 +1,53 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from apps.forms import RegisterForm
+from apps.models import Blog
+
+
+def blog_list_page(request):
+    context = {
+        'blogs': Blog.objects.all()
+    }
+    return render(request, 'apps/blogs/blog-list.html', context)
+
+
+def blog_detail_page(request):
+    context = {
+        'blogs': []
+    }
+    return render(request, 'apps/blogs/blog-detail.html', context)
+
+
+def index_page(request):
+    return render(request, 'apps/index.html')
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('index_page')
+
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if User.objects.filter(username=username).exists():
+            login(request, user)
+            return redirect('index_page')
+
+    return render(request, 'apps/login.html')
+
+
+def register_page(request):
+    form = RegisterForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('index_page')
+    context = {
+        'form': form
+    }
+    return render(request, 'apps/login.html', context)
